a77422e3525821966c343608e6e3b01f40630b14
Jascha Mikolajski <Jaschi@Jaschis-MacBook-Pro.local>
2019-06-18 17:12:21 +0700

    did static

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+// static for everything
+app.use(express.static(path.join(__dirname, 'etsy_clone')))
+
+
+
+
+
 app.listen(3000, () => {
 	console.log('Server is responding to port 3000');
 })
-app.get('/shop',(req,res)=> {
-	res.sendFile(path.join(__dirname, '/etsy_clone/space.txt'))
-	console.log('__dirname', __dirname);
-})
-
-// res.sendFile(path.join(__dirname, '/client/profile.txt'))
-// 	console.log('__dirname', __dirname)
-// })
